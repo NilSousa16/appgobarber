93b3823be7240f457ab6046b67a580907a80b359
NilSousa16 <nil.sousa16@gmail.com>
2020-08-16 17:26:11 -0300

    Created the validations

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,14 @@
 import React, { useCallback, useRef } from 'react';
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput } from 'react-native';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import * as Yup from 'yup';
 
 import { Form } from '@unform/mobile';
 /* Tipagem com os métodos para manipular o formulário de maneira direta */
 import { FormHandles } from '@unform/core';
+
+import getValidationErrors from '../../utils/getValidationErrors'
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -14,6 +17,10 @@ import logoImg from '../../assets/logo.png';
 
 import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
 
+interface SingInFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
   /*
@@ -27,9 +34,55 @@ const SignIn: React.FC = () => {
 
   const navigation = useNavigation();
 
-  const handleSignIn = useCallback((data: object) => {
-    console.log(data);
-  }, []);
+  // Lida com o submit e recebe como parâmetro os dados do formulário
+  const handleSignIn = useCallback(
+    async (data: SingInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        /**
+         * object() - tipo que será validado
+         * shape - defini valildação
+         */
+        const schema = Yup.object().shape({
+          // <Campo a ser validado>: Yup.<tipo>.<validação>.<validação>
+
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+        // Realizando a validação
+        await schema.validate(data, {
+          /** Faz com que seja retornado
+           * todos os erros de validação e
+           * não somente o primeiro encontrado */
+          abortEarly: false,
+        });
+        // await signIn({
+        //   email: data.email,
+        //   password: data.password,
+        // });
+
+        // history.push('/dashboard');
+      } catch (err) {
+        // Erros capturados
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        Alert.alert('Erro na autenticação', 'Ocorreu um erro ao fazer login, cheque as credenciais.')
+
+      }
+    },
+    // variáveis externas devem ser colocadas neste array de dependência
+    [],
+  );
+
+
 
   return (
     <>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,12 @@
-import React, { useRef } from 'react';
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput } from 'react-native';
+import React, { useRef, useCallback } from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -11,6 +14,12 @@ import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
 
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
   /* Para disparar o subimit no form já que o react native não tem um botão submit*/
@@ -21,6 +30,53 @@ const SignUp: React.FC = () => {
   const passwordInputRef = useRef<TextInput>(null);
 
   const navigation = useNavigation();
+
+   // Lida com o submit e recebe como parâmetro os dados do formulário
+   const handleSignUp = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        /**
+         * object() - tipo que será validado
+         * shape - defini valildação
+         */
+        const schema = Yup.object().shape({
+          // <Campo a ser validado>: Yup.<tipo>.<validação>.<validação>
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        });
+        // Realizando a validação
+        await schema.validate(data, {
+          /** Faz com que seja retornado
+           * todos os erros de validação e
+           * não somente o primeiro encontrado */
+          abortEarly: false,
+        });
+
+        // await api.post('/users', data);
+        // /** Redireciona após o cadastro */
+        // history.push('/');
+
+
+      } catch (err) {
+        /** Erros capturados */
+
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        Alert.alert('Erro no cadastro', 'Ocorreu um erro ao fazer cadastro, cheque as credenciais.');
+      }
+    },
+    [],
+  );
 
   return (
     <>
@@ -43,7 +99,7 @@ const SignUp: React.FC = () => {
             <View>
               <Title>Crie sua conta</Title>
             </View>
-            <Form ref={ formRef } onSubmit={(data) => {console.log(data)}}>
+            <Form ref={ formRef } onSubmit={handleSignUp}>
               <Input
                 autoCapitalize="words"
                 name="name"
